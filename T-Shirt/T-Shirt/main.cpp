c786469110ab17d927b6cfc8a461b17c3f68b0f9
Kunlakan (Jeen) Cherdchusilp <kunlakan@uw.edu>
2016-11-14 18:13:52 -0800

    Re-setup the enviroment
    If you have a problem with this enviroment, make sure to look at the
    description of the previous commit :)

--- T-Shirt/T-Shirt/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-using namespace cv; 
-
-int main(int argc, char* argv[]){
-	Mat image;
-}
---
+++ T-Shirt/main.cpp
@@ -0,0 +1,8 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv; 
+
+int main(int argc, char* argv[]){
+	Mat image;
+}
