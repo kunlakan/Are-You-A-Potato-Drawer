e349b548772bd63780479e4f42c587599c74b259
kulsoommansoor <kulsoomm786@gmail.com>
2016-11-24 14:38:36 -0800

    Created separate cropped images of templates and also start TemplateLibrary class
    I took the templates, cropped them and put them in a separate folder
    called "cropped" inside the template_image folder. I also started the
    TemplateLibrary class.

---
+++ T-Shirt/TemplateLibrary.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef __TemplateLibrary__
+#define __TemplateLibrary__
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <string>
+using namespace cv;
+using namespace std;
+
+class TemplateLibrary {
+
+public:
+
+private:
+
+};
+
+#endif /* defined(__TemplateLibrary__) */
